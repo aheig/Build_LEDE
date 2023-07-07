42c6677aee5af96cc7ced0d08364cc0580e0c565
aheig <61814388+aheig@users.noreply.github.com>
2023-07-07 11:02:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -50,7 +50,7 @@ sed -i "s/hostname='.*'/hostname='LEDE'/g" package/base-files/files/bin/config_g
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-1.0修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-1.0'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='LEDE'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（Lan build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 # sed -i "s/OpenWrt /Lan build $(TZ=UTC-8 date "+%Y%m%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
